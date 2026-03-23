f42cded2d437bd6ccb1054aa08f41d923eed0536
devZevitor <vict.95977@gmail.com>
2026-03-23 15:35:32 -0300

    feat(home): refatora hero da Home com novo form e asset responsivo

--- src/componentes/loginHero.tsx
+++ src/componentes/loginHero.tsx
@@ -1,29 +1,46 @@
+import { Form } from "./ui/form";
+
 export function LoginHero() {
     return (
-        <section className="relative overflow-visible h-[50vh] flex flex-col sm:flex-row bg-[#4C9AE4]">
-            <div className="flex flex-1 justify-center sm:justify-start sm:pl-4 items-center ">
-                <form className="w-full max-w-2xl p-4 flex flex-col gap-2">
-                    <h1 className="text-2xl xxs:text-4xl text-white font-bold">Entrar</h1>
-                    <label className="text-white font-semibold">Email </label>
-                    <input className="p-1 bg-white rounded-sm" type="text" name="" id="" placeholder="Email"/>
-                    <label className="text-white font-semibold">Senha </label>
-                    <input className="p-1 bg-white rounded-sm" type="text" name="" id="" placeholder="Senha"/>
-                    <select className="bg-white text-black text-center font-semibold">
-                        <option value="" disabled selected hidden>
-                            Entrar Como
-                        </option>
-                        <option value="Aluno">Aluno</option>
-                        <option value="Professor">Professor</option>
-                        <option value="Administrador">Administrador</option>
-                    </select>
-                    <button className="px-1 text-black font-semibold bg-[#2CD76E]">Entrar</button>
-                </form>
+        <section className="
+            relative overflow-visible  sm:h-[50vh] 
+            flex flex-col-reverse sm:flex-row bg-[radial-gradient(circle_at_30%_40%,#6BAFEF_0%,#4C9AE4_40%,#3F86D1_100%)]
+        ">
+            <div className="flex flex-col flex-1 justify-center items-center p-1 
+                sm:pl-4 
+                md:gap-2
+                lg:justify-between lg:p-2 
+                xl:flex-col xl:items-center xl:justify-end xl:pr-20 xl:p-0 xl:gap-0">
+
+                <h1 className="
+                    text-xs text-center
+                    w-full flex xxs:text-lg items-center justify-center text-wrap font-bold
+                    lg:flex lg:justify-center md:text-2xl  xl:text-3xl
+                ">
+                    Plataforma de Gestão Acadêmica
+                </h1>
+
+                <h1 className="  hidden text-center
+                    w-full font-normal text-xs justify-center
+                    xxs:flex
+                    xs:text-sm
+                    sm:hidden
+                    md:flex md:text-md
+                    lg:text-lg
+                    
+
+                     lg:justify-center
+                ">
+                    Organize seus estudos, acompanhe seu progresso e <br /> gerencie sua jornada universitária em um só lugar.
+                </h1>
+                <Form />
             </div>
-            <div className="flex justify-center items-center sm:pr-4 sm:justify-end sm:items-end flex-1 overflow-visible">
+            <div className="flex justify-center items-center xs:items-center sm:pr-4 sm:justify-end sm:items-end flex-1 overflow-visible xl:justify-center xl:items-center">
                 <div className="
-                    relative z-10 h-full w-36 xxs:w-70 aspect-4/3 shrink-0 
-                    translate-y-4 sm:h-2/3 sm:w-90 sm:translate-y-8 max-w-md bg-[url('/login_img.png')] 
-                    bg-contain bg-center bg-no-repeat" 
+                    relative z-10 h-auto xs:h-full w-42 xxs:w-70 aspect-4/3 shrink-0 
+                    md:translate-y-4 sm:h-2/3 sm:w-90 sm:translate-y-8 max-w-md bg-[url('/login_img.png')] xl:translate-0 xl:bg-[url('/login_img_xl.png')] xl:max-w-lg xl:w-130 xl:h-full
+                    bg-contain bg-center bg-no-repeat
+                    " 
                 />
             </div>
         </section>
