937772ddb51c7e13047b58e259b76de2e83984e1
devZevitor <vict.95977@gmail.com>
2026-05-01 13:32:21 -0300

    feat: add CardPerfil component and integrate it into PaginaFeed; enhance SideBar and CardResumo styles

--- src/paginas/PaginaFeed.tsx
+++ src/paginas/PaginaFeed.tsx
@@ -1,3 +1,4 @@
+import { CardPerfil } from "../componentes/ui/cardPerfil";
 import { CardResumo } from "../componentes/ui/cardResumo";
 
 export function PaginaFeed() {
@@ -74,26 +75,78 @@ export function PaginaFeed() {
         }
     ]
 
+    const seguindo = [
+        {
+            id: "1",
+            nome: 'Fulano',
+            semestre: 4,
+            seguidores: 900,
+        },
+        {
+            id: "2",
+            nome: 'Cliclano',
+            semestre: 6,
+            seguidores: 400,
+        },
+        {
+            id: "3",
+            nome: 'Fulano',
+            semestre: 4,
+            seguidores: 900,
+        },
+        {
+            id: "4",
+            nome: 'Cliclano',
+            semestre: 6,
+            seguidores: 400,
+        }
+    ]
+
     return (
-        <main 
-            className="
-           
-            w-full min-h-screen p-6 mb-20 sm:mb-0
-            sm:grid lg:grid-cols-3 sm:grid-cols-2 flex flex-col sm:grid-flow-dense gap-8 grid-auto-rows-[180px]
-        ">
-           
-            {resumos.map((resumo, index) => {
-                let formato:'quadrado' | 'horizontal' | 'vertical' = "quadrado";
-                if(index % 5 === 0) formato = "horizontal";
-                if(index % 5 === 3) formato = "vertical";
+        <main className="w-full h-full flex justify-around p-16">
+            <section className="
+                w-2/3 min-h-screen p-6 mb-20 sm:mb-0
+                sm:grid sm:grid-cols-2 flex flex-col sm:grid-flow-dense gap-8 grid-auto-rows-[180px] 
+            ">
+                {resumos.map((resumo, index) => {
+                    let formato:'quadrado' | 'horizontal' | 'vertical' = "quadrado";
+                    if(index % 5 === 0) formato = "horizontal";
+                    if(index % 5 === 3) formato = "vertical";
 
-                let cores:("verde" | "salmao" | "rosa" | "azul")[] = ["verde", "salmao", "rosa", "azul"]
-                let cor = cores[index % cores.length]
+                    let cores:("verde" | "salmao" | "rosa" | "azul")[] = ["verde", "salmao", "rosa", "azul"]
+                    let cor = cores[index % cores.length]
 
-                return (
-                    <CardResumo key={resumo.id} titulo={resumo.titulo} texto={resumo.texto} formato={formato} cor={cor}/>
-                )
-            })}
+                    return (
+                        <CardResumo key={resumo.id} titulo={resumo.titulo} texto={resumo.texto} formato={formato} cor={cor}/>
+                    )
+                })}
+            </section>
+
+            <section className={`min-w-96 flex flex-col gap-6 `}>
+                <CardPerfil className="" nome="SeuNome" seguidores={30} semestre={2}/>
+
+                <h2>Seguindo: </h2>
+                {seguindo.map((s) => {
+
+                    return (
+                        <CardPerfil key={s.id} className="" nome={s.nome} seguidores={s.seguidores} semestre={s.semestre}/>
+                    )
+                })}
+
+                <div className="min-h-20 min-w-20 relative z-51 flex gap-1 text-xs">
+                    
+                      <a className="hover:underline" href="#">Sobre</a>
+                      <h2>.</h2>
+                      <a className="hover:underline">Ajuda</a>
+                      <a className="hover:underline" href="#">Contato</a>
+                      <h2>.</h2>
+                      <a className="hover:underline">API</a>
+                      <a className="hover:underline" href="#">Termos</a>
+                      <h2>.</h2>
+                      <a className="hover:underline">Privacidade</a>
+              
+                </div>
+            </section>
         </main>
     )
 }
